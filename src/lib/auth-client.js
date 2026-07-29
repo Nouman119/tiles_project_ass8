338c9530bb43e179c742d63c3acd50629ba24307
Nouman119 <open.faz@gmail.com>
2026-07-29 17:47:40 +0600

    another fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,16 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
-
-const getBaseURL = () => {
-  if (typeof window !== "undefined") {
-    return window.location.origin;
-  }
-  if (process.env.NEXT_PUBLIC_APP_URL) {
-    return process.env.NEXT_PUBLIC_APP_URL;
-  }
-  return "https://tiles-project-ass8-git-main-nouman18.vercel.app";
-};
-
 export const authClient = createAuthClient({
-  baseURL: getBaseURL(),
+  baseURL: typeof window !== "undefined" ? window.location.origin : "https://tiles-project-ass8-git-main-nouman18.vercel.app",
 });
