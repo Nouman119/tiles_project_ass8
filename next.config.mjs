49ce4c81f73fb6e5a6df83833ef419a6c527daaf
Nouman119 <Open.faz@gmail.com>
2026-07-30 04:06:47 +0600

    next js config edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
     return [
       {
         source: '/auth-proxy/:path*',
-        destination: `${baseUrl}/api/auth/:path*`,
+        destination: '/api/auth/:path*',
       },
     ];
   },
